ccdf2081fb217ce5b2480e316a527c7f9f5dec81
monika <monika.rajoriya1@gmail.com>
2018-12-20 10:54:55 +0530

    Added Hello Monika

--- src/p1/HelloWorld.java
+++ src/p1/HelloWorld.java
@@ -5,10 +5,9 @@ public class HelloWorld {
 	public static void main(String args[])
 	{
 		System.out.println("Hello World!");
-
 		System.out.println("Monika Loves chole bhature!");
-
-		System.out.println("Hello World!");
+		System.out.println("Hello Word!");
+		System.out.println("Hello Monika");
 
 	
 	}
